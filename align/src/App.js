9a9f1580a09b14196f0eaf52933e1de461b0acbc
Samantha <sammierosado@gmail.com>
2020-07-16 17:15:25 -0400

    loader

--- align/src/App.js
+++ align/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {BounceLoader} from 'react-spinners'
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./components/Routes";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import { Spinner } from "react-bootstrap";
 
 function App() {
   return (
--- align/src/components/DailyHoroscope.js
+++ align/src/components/DailyHoroscope.js
@@ -1,34 +1,41 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import "../App.css";
+import { PuffLoader } from "react-spinners";
 
 function DailyHoroscope() {
   const [sunSignHoro, setSunSignHoro] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  const URL = "https://aztro.sameerkumar.website/?sign=aries&day=today";
-  fetch(URL, {
+  const getHoroscopeData = () => {
+    const URL = "https://aztro.sameerkumar.website/?sign=gemini&day=today";
+    fetch(URL, {
     method: "POST",
   })
     .then((response) => response.json())
     .then((json) => {
       setSunSignHoro(json);
+      setLoading(true);
     });
+  }
 
+  useEffect(() => {
+   getHoroscopeData();
+  });
   return (
     <div>
-      DailyHoroscope
-      <div>
-      {sunSignHoro.current_date}
+      <div className="horoscopeHeader">Gemini Horoscope</div>
+      <div className="horoscopePage">
+        <div>{sunSignHoro.current_date}</div>
+        <div>{sunSignHoro.description}</div>
+        {sunSignHoro.compatibility}
+        {sunSignHoro.lucky_number}
+        {sunSignHoro.lucky_time}
+        {sunSignHoro.color}
+        {sunSignHoro.date_range}
+        {sunSignHoro.mood}
+        <PuffLoader size={150} color={"#123abc"} />
       </div>
-      <div>
-      {sunSignHoro.description}
-      </div>
-      
-      {sunSignHoro.compatibility}
-      {sunSignHoro.lucky_number}
-      {sunSignHoro.lucky_time}
-      {sunSignHoro.color}
-      {sunSignHoro.date_range}
-      {sunSignHoro.mood}
     </div>
   );
 }
